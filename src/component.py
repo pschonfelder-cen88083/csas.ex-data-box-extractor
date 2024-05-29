f1ac142d7eddd66c9f93a9ab102b6f4a16b8eac8
pschonfelder-cen88083 <86102631+pschonfelder-cen88083@users.noreply.github.com>
2024-05-29 15:12:08 +0200

    fix: basic load functionality
    load 3 url from public URI, tranform it and store it as output.csv table

--- src/component.py
+++ src/component.py
@@ -1,13 +1,20 @@
 """
-Template Component main class.
+Data-box component main class.
 
 """
 import csv
 import logging
-from datetime import datetime
 
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
+
+
+from src.data_box import load_ovm
+from src.data_box import load_pfo
+from src.data_box import load_po
+from src.data_box import get_data_columns
+
+
 
 # configuration variables
 KEY_API_TOKEN = '#api_token'
@@ -15,7 +22,8 @@ KEY_PRINT_HELLO = 'print_hello'
 
 # list of mandatory parameters => if some is missing,
 # component will fail with readable message on initialization.
-REQUIRED_PARAMETERS = [KEY_PRINT_HELLO]
+#REQUIRED_PARAMETERS = [KEY_PRINT_HELLO]
+REQUIRED_PARAMETERS = []
 REQUIRED_IMAGE_PARS = []
 
 
@@ -44,44 +52,39 @@ class Component(ComponentBase):
         self.validate_image_parameters(REQUIRED_IMAGE_PARS)
         params = self.configuration.parameters
         # Access parameters in data/config.json
-        if params.get(KEY_PRINT_HELLO):
-            logging.info("Hello World")
+        # if params.get(KEY_PRINT_HELLO):
+        #     logging.info("Hello World")
 
         # get input table definitions
-        input_tables = self.get_input_tables_definitions()
-        for table in input_tables:
-            logging.info(f'Received input table: {table.name} with path: {table.full_path}')
-
-        if len(input_tables) == 0:
-            raise UserException("No input tables found")
+        # input_tables = self.get_input_tables_definitions()
+        # for table in input_tables:
+        #     logging.info(f'Received input table: {table.name} with path: {table.full_path}')
+        #
+        # if len(input_tables) == 0:
+        #     raise UserException("No input tables found")
 
         # get last state data/in/state.json from previous run
         previous_state = self.get_state_file()
         logging.info(previous_state.get('some_state_parameter'))
 
         # Create output table (Tabledefinition - just metadata)
-        table = self.create_out_table_definition('output.csv', incremental=True, primary_key=['timestamp'])
+        # table = self.create_out_table_definition('output.csv', incremental=True, primary_key=['timestamp'])
+        table = self.create_out_table_definition('output.csv', incremental=True, primary_key=['id'], columns= get_data_columns())
 
         # get file path of the table (data/out/tables/Features.csv)
         out_table_path = table.full_path
         logging.info(out_table_path)
 
-        # Add timestamp column and save into out_table_path
-        input_table = input_tables[0]
-        with open(input_table.full_path, 'r') as inp_file, \
-                open(table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
-            reader = csv.DictReader(inp_file)
-
-            columns = list(reader.fieldnames)
-            # append timestamp
-            columns.append('timestamp')
-
+        # input_table = input_tables[0]
+        with open(table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
+            columns = get_data_columns()
             # write result with column added
-            writer = csv.DictWriter(out_file, fieldnames=columns)
-            writer.writeheader()
-            for in_row in reader:
-                in_row['timestamp'] = datetime.now().isoformat()
-                writer.writerow(in_row)
+            dict_writer = csv.DictWriter(out_file, fieldnames=columns)
+            dict_writer.writeheader()
+            lambda_writer = lambda data: dict_writer.writerow(data)
+            load_po(lambda_writer)
+            load_ovm(lambda_writer)
+            load_pfo(lambda_writer)
 
         # Save table manifest (output.csv.manifest) from the tabledefinition
         self.write_manifest(table)
---
+++ src/data_box.py
@@ -0,0 +1,87 @@
+import requests
+import xml.etree.ElementTree as ET
+import io
+
+namespace = 'http://seznam.gov.cz/ovm/datafile/seznam_ds/v1'
+ET.register_namespace('', namespace)
+ns = '{' + namespace + '}'
+
+
+# class DataWriter:
+#     def write(sefl, data: dict):
+#         print(data)
+
+
+def url_data_dtream_as_file(url):
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        return io.BytesIO(response.content)
+    else:
+        raise Exception(f'Failed to download url:{url} response {response.status_code}')
+
+
+def get_data_columns() -> list:
+    return [string.upper() for string in get_exact_xml_names()]
+
+def get_exact_xml_names() -> list:
+    return ['id', 'type', 'subtype', 'tradeName', 'ico', 'isMaster', 'masterId']
+
+
+def element_to_attr_name() -> dict:
+    """ XML element name (with namespace) to result column name  """
+    mapping = {}
+    for col_name in get_exact_xml_names():
+        mapping[ns + col_name] = col_name.upper()
+    return mapping
+
+def load_data(file, data_writer) -> int:
+    """ data_writer is function processing dict parameter"""
+    mapping : dict = element_to_attr_name()
+    count = 0
+    box_attributes = {}
+    for event, elem in ET.iterparse(file):
+        if elem.tag == ns + 'box':
+            data_writer(box_attributes)
+            box_attributes = {}  # Reset for next box
+            count = count + 1
+        else:
+            if elem.tag in mapping:
+                colum_name = mapping[elem.tag]
+                box_attributes[colum_name] = elem.text
+        elem.clear()  # uvolňuje paměť
+    return count
+
+
+def load_po(data_writer) -> int:
+    # Právnické osoby	https://www.mojedatovaschranka.cz/sds/datafile?format=xml&service=seznam_ds_po	653 MB
+    url_po = "https://www.mojedatovaschranka.cz/sds/datafile?format=xml&service=seznam_ds_po"
+    with url_data_dtream_as_file(url_po) as file:
+        count = load_data(file=file, data_writer=data_writer)
+        print(f"Load PO {url_po} count {count}")
+        return count
+
+
+def load_pfo(data_writer) -> int:
+    # Podnikající fyzické osoby	https://www.mojedatovaschranka.cz/sds/datafile?format=xml&service=seznam_ds_pfo	5 MB
+    url_pfo = "https://www.mojedatovaschranka.cz/sds/datafile?format=xml&service=seznam_ds_pfo"
+    with url_data_dtream_as_file(url_pfo) as file:
+        count = load_data(file=file, data_writer=data_writer)
+        print(f"Load PFO {url_pfo} count {count}")
+        return count
+
+
+def load_ovm(data_writer) -> int:
+    # Orgány veřejné moci	https://www.mojedatovaschranka.cz/sds/datafile?format=xml&service=seznam_ds_ovm	16 MB
+    url_ovm = "https://www.mojedatovaschranka.cz/sds/datafile?format=xml&service=seznam_ds_ovm"
+    with url_data_dtream_as_file(url_ovm) as file:
+        count = load_data(file=file, data_writer=data_writer)
+        print(f"Load OWM {url_ovm} count {count}")
+        return count
+
+
+# udage example
+
+#load_po(dataWriter)
+#load_pfo(print)
+#load_ovm(dataWriter)
+
